8c4352e0421468b72d173c578c3fab06a704a791
Edward Casbon <edward@edwardcasbon.co.uk>
2023-09-06 08:51:55 +0100

    Added router

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,11 +2,15 @@ import "./app.scss";
 
 import React from "react";
 import { AppContextProvider } from "./AppContext";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { routes } from "./routes";
+
+const router = createBrowserRouter(routes);
 
 function App(): JSX.Element {
     return (
         <AppContextProvider>
-            <React.Fragment></React.Fragment>
+            <RouterProvider router={router} />
         </AppContextProvider>
     );
 }
