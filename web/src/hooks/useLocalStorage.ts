b681dd96648ef81b87c52972e40f7cf288a6efd3
Edward Casbon <edward@edwardcasbon.co.uk>
2023-09-11 12:13:18 +0100

    Added a hook for using localstorage

---
+++ web/src/hooks/useLocalStorage.ts
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+
+export const useLocalStorage = (
+    key: string = ""
+): [value: any, setValue: React.Dispatch<any>] => {
+    const [value, setValue] = useState(
+        JSON.parse(localStorage.getItem(key)) ?? null
+    );
+
+    useEffect(() => {
+        localStorage.setItem(key, JSON.stringify(value));
+    }, [value, key]);
+
+    return [value, setValue];
+};
