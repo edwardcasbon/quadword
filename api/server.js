c315062b91faee0c3a75d74cc95b825e94e68268
Edward Casbon <edward@edwardcasbon.co.uk>
2023-09-13 13:42:42 +0100

    Enable CORS requests on the server

--- api/server.js
+++ api/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const endpoints = require("./endpoints/index");
 
 const app = express();
 const port = 3000;
+
+app.use(cors());
 
 endpoints.forEach((endpoint) => {
     app.use(endpoint.path, endpoint.router);
