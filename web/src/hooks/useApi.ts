a453f0c6487a7de0c37ea68b5ac6994fdbdfb284
Edward Casbon <edward@edwardcasbon.co.uk>
2023-09-15 11:38:49 +0100

    Added a fetchApi function and used within hook

--- web/src/hooks/useApi.ts
+++ web/src/hooks/useApi.ts
@@ -1,11 +1,10 @@
 import React, { useEffect } from "react";
-
-const apiUrl = "http://localhost:3000";
+import { fetchApi } from "../fetches/api";
 
 export const useApi = (
     uri: string,
     callback: (json: any) => void,
-    dependencies: [] = [],
+    dependencies: any[] = [],
     method: string = "GET",
     data: any = null
 ) => {
@@ -15,29 +14,8 @@ export const useApi = (
 
     useEffect(() => {
         const controller = new AbortController();
-        const signal = controller.signal;
 
-        const fetchOptions: {
-            signal: AbortSignal;
-            method: string;
-            body?: any;
-        } = {
-            signal,
-            method,
-        };
-
-        if ((method === "POST" || method === "PUT") && data !== null) {
-            fetchOptions.body = JSON.stringify(data);
-        }
-
-        fetch(`${apiUrl}${uri}`, fetchOptions)
-            .then((response) => response.json())
-            .then((json) => {
-                if (typeof callback === "function") {
-                    callback.call(this, json);
-                }
-            })
-            .catch((err) => console.error(err));
+        fetchApi(uri, callback, method, data, controller);
 
         return () => {
             controller.abort();
