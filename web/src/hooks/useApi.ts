6d2ef1fe27f2ddeeb1d8159ead79a6c133b7c11e
Edward Casbon <edward@edwardcasbon.co.uk>
2023-09-15 11:03:51 +0100

    Updated useApi hook to include method and data options

--- web/src/hooks/useApi.ts
+++ web/src/hooks/useApi.ts
@@ -5,7 +5,9 @@ const apiUrl = "http://localhost:3000";
 export const useApi = (
     uri: string,
     callback: (json: any) => void,
-    dependencies: [] = []
+    dependencies: [] = [],
+    method: string = "GET",
+    data: any = null
 ) => {
     if (!Array.isArray(dependencies)) {
         dependencies = [];
@@ -15,7 +17,20 @@ export const useApi = (
         const controller = new AbortController();
         const signal = controller.signal;
 
-        fetch(`${apiUrl}${uri}`, { signal })
+        const fetchOptions: {
+            signal: AbortSignal;
+            method: string;
+            body?: any;
+        } = {
+            signal,
+            method,
+        };
+
+        if ((method === "POST" || method === "PUT") && data !== null) {
+            fetchOptions.body = JSON.stringify(data);
+        }
+
+        fetch(`${apiUrl}${uri}`, fetchOptions)
             .then((response) => response.json())
             .then((json) => {
                 if (typeof callback === "function") {
