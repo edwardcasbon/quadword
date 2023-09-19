3173cb1cd3a8b0db37ad9d85f0bc8293dfd8e08f
Edward Casbon <edward@edwardcasbon.co.uk>
2023-09-19 14:40:02 +0100

    Added access keys

--- web/src/components/game/Keyboard.tsx
+++ web/src/components/game/Keyboard.tsx
@@ -24,6 +24,7 @@ export default function Keyboard({
                             onClick={() => {
                                 onUpdate(key);
                             }}
+                            accessKey={key}
                         >
                             {key === "backspace" ? (
                                 <span className="material-symbols-outlined">
