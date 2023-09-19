0afc4e93f0f8d162692afb36b544e5acf953fe89
Edward Casbon <edward@edwardcasbon.co.uk>
2023-09-19 14:32:44 +0100

    Added active states to footer

--- web/src/components/footer/Menu.tsx
+++ web/src/components/footer/Menu.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { IMenuButton } from "../../types/footer/menu";
 
 const items: IMenuButton[] = [
@@ -20,21 +20,34 @@ const items: IMenuButton[] = [
     },
 ];
 
+function isItemActive(item: IMenuButton, pathname: string) {
+    if (item.route === "/" && ["/game", "/start"].find((p) => p === pathname)) {
+        return true;
+    }
+
+    return item.route === pathname;
+}
+
 export default function Menu() {
+    const { pathname } = useLocation();
+
     return (
         <ul>
             {items.map((item) => (
                 <li key={item.label.toLowerCase()}>
-                    <MenuButton item={item} />
+                    <MenuButton
+                        item={item}
+                        active={isItemActive(item, pathname)}
+                    />
                 </li>
             ))}
         </ul>
     );
 }
 
-function MenuButton({ item }: { item: IMenuButton }) {
+function MenuButton({ item, active }: { item: IMenuButton; active: boolean }) {
     return (
-        <Link to={item.route}>
+        <Link to={item.route} className={active ? "active" : ""}>
             <span className="material-symbols-outlined">{item.icon}</span>
             {item.label}
         </Link>
