74916f4e7197ec8bf3801d6a578bfd39f6531245
Edward Casbon <edward@edwardcasbon.co.uk>
2023-09-19 14:35:43 +0100

    Added active state to header

--- web/src/components/header/User.tsx
+++ web/src/components/header/User.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { useAppContext } from "../../AppContext";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function User() {
     const { user } = useAppContext();
+    const { pathname } = useLocation();
 
     if (!user?.name) {
         return null;
     }
 
     return (
-        <Link to="/user">
+        <Link to="/user" className={pathname === "/user" ? "active" : ""}>
             <span className="material-symbols-outlined">account_circle</span>
         </Link>
     );
