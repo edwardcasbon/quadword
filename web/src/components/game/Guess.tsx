58f54b63a003081d0b9d6e310b5ea876918a7120
Edward Casbon <edward@edwardcasbon.co.uk>
2023-09-19 11:53:10 +0100

    Additional styling

--- web/src/components/game/Guess.tsx
+++ web/src/components/game/Guess.tsx
@@ -3,7 +3,10 @@ import React from "react";
 export default function Guess({ guess }: { guess: string }) {
     return (
         <div className="game__guess">
-            <p>{guess}</p>
+            <span>{guess[0]}</span>
+            <span>{guess[1]}</span>
+            <span>{guess[2]}</span>
+            <span>{guess[3]}</span>
         </div>
     );
 }
