3456a2b40c037b312ccda0cb043aacf5c78ae73f
Edward Casbon <edward@edwardcasbon.co.uk>
2023-09-20 12:17:18 +0100

    Added cursor

--- web/src/components/game/Guess.tsx
+++ web/src/components/game/Guess.tsx
@@ -3,10 +3,14 @@ import React from "react";
 export default function Guess({ guess }: { guess: string }) {
     return (
         <div className="game__guess">
-            <span>{guess[0]}</span>
-            <span>{guess[1]}</span>
-            <span>{guess[2]}</span>
-            <span>{guess[3]}</span>
+            {[0, 1, 2, 3].map((index) => (
+                <div
+                    key={index}
+                    className={index === guess.length ? "cursor" : ""}
+                >
+                    {guess[index]}
+                </div>
+            ))}
         </div>
     );
 }
