c1436f9a284ff6626f4a18acd9a641cdc5b55a68
Edward Casbon <edward@edwardcasbon.co.uk>
2023-09-21 14:45:16 +0100

    Trigger animation when lose lives

--- web/src/components/game/Lives.tsx
+++ web/src/components/game/Lives.tsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function Lives({ lives }: { lives: number }) {
+    const [isAnimating, setIsAnimating] = useState(false);
+
+    useEffect(() => {
+        setIsAnimating(true);
+    }, [lives]);
+
     return (
-        <p className="game__lives">
+        <p
+            className={`game__lives ${
+                isAnimating ? "game__lives--shrink" : ""
+            }`}
+            onAnimationEnd={() => {
+                setIsAnimating(false);
+            }}
+        >
             {lives} <span className="material-symbols-outlined">favorite</span>
         </p>
     );
