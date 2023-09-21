c23fcdcb42934ffeed390c5d5c9f01b1c8aab538
Edward Casbon <edward@edwardcasbon.co.uk>
2023-09-21 14:40:51 +0100

    Added grow animation to points

--- web/src/components/game/Points.tsx
+++ web/src/components/game/Points.tsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Points({ points }: { points: number }) {
+    const [isAnimating, setIsAnimating] = useState(false);
+
+    useEffect(() => {
+        setIsAnimating(true);
+    }, [points]);
+
     return (
-        <p className="game__points">
+        <p
+            className={`game__points ${
+                isAnimating ? "game__points--grow" : ""
+            }`}
+            onAnimationEnd={() => {
+                setIsAnimating(false);
+            }}
+        >
             <span className="material-symbols-outlined">brightness_high</span>
             {points}{" "}
         </p>
