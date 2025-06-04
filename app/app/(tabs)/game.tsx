6cb650ab6bf97440d6a6855016eb24520486503d
Edward Casbon <edward@edwardcasbon.co.uk>
2025-06-04 13:39:57 +0100

    Ensure user is signed in when on game page

--- app/app/(tabs)/game.tsx
+++ app/app/(tabs)/game.tsx
@@ -1,10 +1,29 @@
+import { useRouter } from "expo-router";
+import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
+import Loading from "@/components/Loading";
 import Styles from "@/styles/Styles";
+import { IUser } from "@/types/User";
+import { getCurrentUser } from "@/utilities/User";
 
 export default function Tab() {
+    const router = useRouter();
+    const [isLoadingUser, setIsLoadingUser] = useState(true);
+    const [user, setUser] = useState<IUser | null>(null);
+
+    useEffect(() => {
+        (async () => {
+            const user = await getCurrentUser();
+            if (!user) {
+                router.replace("/sign-in");
+            }
+            setUser(user);
+            setIsLoadingUser(false);
+        })();
+    });
     return (
         <View style={Styles.container}>
-            <Text>Game</Text>
+            {isLoadingUser ? <Loading /> : <Text>Game</Text>}
         </View>
     );
 }
