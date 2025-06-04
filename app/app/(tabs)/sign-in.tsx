2de2a884827be28ab94f49b03431fe05e40018da
Edward Casbon <edward@edwardcasbon.co.uk>
2025-06-04 13:39:33 +0100

    Added sign in and sign out functionality to pages

--- app/app/(tabs)/sign-in.tsx
+++ app/app/(tabs)/sign-in.tsx
@@ -1,10 +1,47 @@
-import { Text, View } from "react-native";
+import { useRouter } from "expo-router";
+import { useState } from "react";
+import {
+    Button,
+    KeyboardAvoidingView,
+    Text,
+    TextInput,
+    View,
+} from "react-native";
 import Styles from "@/styles/Styles";
+import { setCurrentUser } from "@/utilities/User";
 
-export default function Index() {
+export default function Tab() {
+    const router = useRouter();
+    const [name, setName] = useState("");
+
     return (
         <View style={Styles.container}>
-            <Text>Sign in</Text>
+            <KeyboardAvoidingView
+                behavior="padding"
+                style={Styles.innerContainer}
+            >
+                <Text>Name</Text>
+                <TextInput
+                    inputMode="text"
+                    onChangeText={(text) => {
+                        setName(text);
+                    }}
+                    style={{
+                        height: 40,
+                        borderWidth: 1,
+                        padding: 10,
+                    }}
+                />
+                <Button
+                    title="Sign in"
+                    onPress={async () => {
+                        if (name !== "") {
+                            await setCurrentUser({ name });
+                            router.push("/game");
+                        }
+                    }}
+                />
+            </KeyboardAvoidingView>
         </View>
     );
 }
--- app/app/(tabs)/sign-out.tsx
+++ app/app/(tabs)/sign-out.tsx
@@ -1,10 +1,25 @@
-import { Text, View } from "react-native";
+import { useRouter } from "expo-router";
+import { useEffect } from "react";
+import { View } from "react-native";
+import Loading from "@/components/Loading";
 import Styles from "@/styles/Styles";
+import { setCurrentUser } from "@/utilities/User";
 
-export default function Index() {
+export default function Tab() {
+    const router = useRouter();
+
+    // Sign the user out (aka set the current user to null), then redirect to
+    // the account page where it'll likely redirect back to the sign in page.
+    useEffect(() => {
+        (async () => {
+            await setCurrentUser(null);
+            router.replace("/account");
+        })();
+    }, [router]);
+
     return (
         <View style={Styles.container}>
-            <Text>Sign out</Text>
+            <Loading />
         </View>
     );
 }
